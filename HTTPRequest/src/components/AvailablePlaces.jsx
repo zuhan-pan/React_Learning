fb18ac8e2566f80f789f1363579304950e806992
zuhan <harrypan111@gmail.com>
2024-12-10 02:27:07 +0000

    HTTP request and Custom Hooks

---
+++ HTTPRequest/src/components/AvailablePlaces.jsx
@@ -0,0 +1,50 @@
+import Places from './Places.jsx';
+import { useState, useEffect } from 'react';
+import ErrorMessage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const resData = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition(position => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError(error);
+        setIsFetching(false);
+      }
+    }
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <ErrorMessage title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data"
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
---
+++ HTTPRequest/src/http.js
@@ -0,0 +1,25 @@
+export async function fetchAvailablePlaces() {
+  const response = await fetch('http://localhost:3000/places');
+  const resData = await response.json();
+
+  if (!response.ok) throw new Error('Failed to fetch places');
+
+  return resData;
+}
+
+export async function updatedUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to update user data');
+  }
+  return resData.message;
+}
